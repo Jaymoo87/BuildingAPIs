e3e17f7c80724d987f296e316adf58c4acb06495
Justin Murrah <jdmurrah@gmail.com>
2022-11-29 21:09:17 -0600

    initial commit. chirps are sorted out and postman confirms the endpoint functionality

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const apiRouter = require("./routes");
+
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../client")));
+app.use("/api", apiRouter);
+
+app.listen(3000, () => console.log("Server Connected (3000)"));
